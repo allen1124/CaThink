74a75c24fde54afa5fb1b54faf42664a7066fd45
allen1124 <alantsui32238@gmail.com>
2018-03-24 05:36:26 +0800

    Create Images app

---
+++ images/models.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Image(models.Model):
+
+	title = models.CharField(max_length=120)
+	image = models.ImageField(null=True, blank=True)
+	description = models.TextField()
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+	def __unicode__(self):
+		return self.title
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse("images:detail", kwargs={"id": self.id})
+
+	class Meta:
+		ordering = ["-timestamp", "-updated"]
+
