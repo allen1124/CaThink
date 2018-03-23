fd9ddbf0ce66ea6788805e9bea0776eab23710f1
allen1124 <alantsui32238@gmail.com>
2018-03-24 00:33:17 +0800

    no message

---
+++ ImageX/views.py
@@ -0,0 +1,38 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/index/')
+
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/index/')
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('login')
+    return render(request, 'registration/register.html', {'form': form})
+
+
