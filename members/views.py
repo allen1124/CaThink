f87956acb55e06616858136eb68ca0022c87a990
Peter Po <peter08po@gmail.com>
2018-04-03 18:36:32 +0800

    Random

--- members/views.py
+++ members/views.py
@@ -9,50 +9,50 @@ from members.models import Profile
 
 
 @login_required
-def profile_detail(request, id = None):
-	profile = get_object_or_404(Profile, id=id)
-	user = profile.user
-	context = {
-		"profile": profile,
-		"user_": user,
-	}
-	return render(request, "profile_detail.html", context)
+def profile_detail(request, id=None):
+    profile = get_object_or_404(Profile, id=id)
+    user = profile.user
+    context = {
+        "profile": profile,
+        "user_": user,
+    }
+    return render(request, "profile_detail.html", context)
 
 
 @login_required
 @transaction.atomic
 def profile_edit(request):
-	if request.method == 'POST':
-		user_form = UserForm(request.POST or None, instance=request.user)
-		profile_form = ProfileForm(request.POST or None, instance=request.user.profile, files=request.FILES)
-		if user_form.is_valid() and profile_form.is_valid():
-			user_form.save()
-			profile_form.save()
-			messages.success(request, 'Your profile was successfully updated!')
-			return redirect('profile_detail', request.user.id)
-		else:
-			messages.error(request, 'Please correct the error below.')
-	else:
-		user_form = UserForm(instance=request.user)
-		profile_form = ProfileForm(instance=request.user.profile)
-		return render(request, 'profile_form.html', {
-			'user_form': user_form,
-			'profile_form': profile_form
-			})
+    if request.method == 'POST':
+        user_form = UserForm(request.POST or None, instance=request.user)
+        profile_form = ProfileForm(request.POST or None, instance=request.user.profile, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('profile_detail', request.user.id)
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+        return render(request, 'profile_form.html', {
+            'user_form': user_form,
+            'profile_form': profile_form
+        })
 
 
 def change_password(request):
-	if request.method == 'POST':
-		form = PasswordChangeForm(request.user, request.POST)
-		if form.is_valid():
-			form.save()
-			update_session_auth_hash(request, form.user)
-			messages.success(request, 'Your Password was successfully updated!')
-			return redirect('profile_detail', request.user.id)
-		else:
-			messages.error(request, 'Please correct the error below.')
-	else:
-		form = PasswordChangeForm(request.user)
-	return render(request, 'profile_change_password.html', {
-		'form': form
-	})
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your Password was successfully updated!')
+            return redirect('profile_detail', request.user.id)
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'profile_change_password.html', {
+        'form': form
+    })
