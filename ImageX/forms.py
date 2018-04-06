66de4d7c16805b50287e71204188dee6f504ff07
Allen Tsui <allentsui@TsuideMacBook-Pro.local>
2018-04-06 16:59:58 +0800

    Search function

--- ImageX/forms.py
+++ ImageX/forms.py
@@ -5,21 +5,21 @@ from members.models import Profile
 
 
 class SignupForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password1', 'password2')
+	class Meta:
+		model = User
+		fields = ('username', 'password1', 'password2')
 
 
 class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
+	class Meta:
+		model = User
+		fields = ('first_name', 'last_name', 'email')
 
 
 class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('avatar', 'bio', 'location')
+	class Meta:
+		model = Profile
+		fields = ('avatar', 'bio', 'location')
 
 class InvitationForm(forms.Form):
 	email = forms.EmailField(max_length=200, help_text='Required')
--- images/views.py
+++ images/views.py
@@ -4,16 +4,27 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .forms import ImageForm
 from .models import Image
+from django.contrib.auth.models import User
 
 
 def image_search(request):
 	queryset_list = Image.objects.all()
-	query = request.GET.get("q")
-	category = request.GET.get("cat")
+	query_string_q = request.GET.get("q")
+	query_string_p = request.GET.get("p")
+	query_category = request.GET.get("cat")
+	query = None
+	if query_string_q:
+		query = Q(tag=query_string_q)
+	elif query_string_p:
+		try:
+			user = User.objects.get(username=query_string_p)
+		except User.DoesNotExist:
+			pass
+		query = Q(user=user)
+	if query_category:
+		query = query & Q(category=query_category)
 	if query:
-		queryset_list = queryset_list.filter(Q(tag=query))
-	if category:
-		queryset_list = queryset_list.filter(Q(category=category))
+		queryset_list = queryset_list.filter(query)
 	context = {
 		"imagesList": queryset_list,
 	}
