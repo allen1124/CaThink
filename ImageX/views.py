211683ffaa815ef792331280b9be26635f0fb734
Allen Tsui <allentsui@TsuideMacBook-Pro.local>
2018-04-24 17:53:53 +0800

    1st image create at index page

--- ImageX/views.py
+++ ImageX/views.py
@@ -13,9 +13,14 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from images.models import Image
 from django.db.models import F, Count
+from django.core.files.images import ImageFile
 
 
 def index(request):
+    if not Image.objects.filter(pk=1).exists():
+        image_ = Image.objects.create(user=get_object_or_404(User, id=1))
+        image_.image = ImageFile(open("static/img/bg-masthead.jpg", "rb"))
+        image_.save()
     liked_images = None
     queryset_list = Image.objects.all().exclude(id=1).annotate(
         popularity=(F('download_count')+Count('likes'))).order_by('-popularity') [:10]
