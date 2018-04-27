a969b2ff2229c38740434cf88eb12d0991ae62f4
Allen Tsui <allentsui@TsuideMacBook-Pro.local>
2018-04-27 11:08:01 +0800

    debug

--- ImageX/views.py
+++ ImageX/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from images.models import Image
 from django.db.models import F, Count
